e58c1afda97cb2454eae2767c371a89c8208895e
edu-muller <ledumuller@gmail.com>
2021-03-17 00:46:08 -0300

    Inicio atrasado

---
+++ src/components/ReactTable.js
@@ -0,0 +1,174 @@
+// Credit https://medium.com/@blaiseiradukunda/react-table-7-tutorial-3d8ba6ac8b16
+import React from "react";
+import { useTable, useSortBy, useFilters, useGlobalFilter, useFlexLayout, useResizeColumns } from "react-table";
+import CustomInput from "./CustomInput";
+
+window.Date.prototype.isValid = function () {
+  // An invalid date object returns NaN for getTime() and NaN is the only
+  // object not strictly equal to itself.
+  // eslint-disable-next-line
+  return this.getTime() === this.getTime();
+};
+
+// value and onChange function
+const GlobalFilter = ({ globalFilter, setGlobalFilter }) => {
+  return (
+    <CustomInput
+      value={globalFilter || ""}
+      onChange={e => {
+        setGlobalFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search All ...`}
+    />
+  );
+};
+
+const ColumnFilter = ({ column: { filterValue, setFilter, filter } }) => {
+  return (
+    <CustomInput
+      value={filterValue || ""}
+      onChange={e => {
+        setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${filter ? filter : ""}...`}
+    />
+  );
+};
+
+/**
+ * As in the previous versions, any react table needs colums where at the core we have a field Header, and accessor
+ * As in the previous versions, a react table has data that consist of an array of JSONs
+ */
+const ReactTable = ({ columns, data }) => {
+  // functions to run when a column is filtered depending on the type
+  const filterTypes = {
+    year: (rows, id, filterValue) => {
+      return rows.filter(row => {
+        const rowValue = row.values[id];
+        return rowValue !== undefined &&
+          Number(filterValue) &&
+          new Date(rowValue) &&
+          new Date(rowValue).isValid()
+          ? new Date(rowValue).getFullYear() === Number(filterValue)
+          : true;
+      });
+    },
+    text: (rows, id, filterValue) => {
+      return rows.filter(row => {
+        const rowValue = row.values[id];
+        return rowValue !== undefined
+          ? String(rowValue)
+            .toLowerCase()
+            .startsWith(String(filterValue).toLowerCase())
+          : true;
+      });
+    }
+  };
+  const defaultColumn = React.useMemo(() => ({
+    // Let's set up our default Filter UI
+    Filter: ColumnFilter,
+    minWidth: 40,
+    maxWidth: 400
+  }), [])
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+      filterTypes
+    },
+    // hooks for filters
+    useFilters,
+    useGlobalFilter,
+    // hook for sorting
+    useSortBy,
+    // hooks for resizing
+    useFlexLayout,
+    useResizeColumns
+  );
+  return (
+    <div>
+      <div
+        className="p-1 border-0 d-flex justify-content-end"
+      >
+        <GlobalFilter
+          globalFilter={state.globalFilter}
+          setGlobalFilter={setGlobalFilter}
+        />
+      </div>
+      <div className='table' {...getTableProps()}>
+        <div >
+          {headerGroups.map(headerGroup => (
+            <div {...headerGroup.getHeaderGroupProps()} className='tr'>
+              {headerGroup.headers.map((column, i) => {
+                // three new addition to column: isSorted, isSortedDesc, getSortByToggleProps
+                const {
+                  render,
+                  getHeaderProps,
+                  isSorted,
+                  isSortedDesc,
+                  getSortByToggleProps,
+                  // filter,
+                  canFilter,
+                  //resizer
+                  isResizing,
+                  getResizerProps                
+                } = column;
+                const extraClass = isSorted
+                  ? isSortedDesc
+                    ? "desc"
+                    : "asc"
+                  : "";
+                const {onClick , ...rest} = getHeaderProps(getSortByToggleProps())
+                console.log('click', onClick, 'rest',rest)
+                return (
+                  <div
+                    key={`th-${i}`}
+                    className={`${extraClass} th`}
+                    {...rest}
+                  // getHeaderProps now receives a function
+                  >
+                    <div onClick={onClick}>
+                      {render("Header")}
+                    </div>
+                    {/* resizer div */}
+                    <div
+                      {...getResizerProps()}
+                      className={`resizer ${isResizing ? 'isResizing' : ''}`}
+                    />
+                    {/* Render the columns filter UI */}
+                    <div>{canFilter ? render("Filter") : null}</div>
+                  </div>
+                );
+              })}
+            </div>
+          ))}
+        </div>
+        <div {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <div {...row.getRowProps()} className="tr">
+                {row.cells.map(cell => {
+                  return (
+                    <div {...cell.getCellProps()} className="td">{cell.render("Cell")}</div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReactTable;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import "./App.css";
 
 import Highcharts from "highcharts";
 import HighchartsMap from "highcharts/modules/map";
 import HighchartsReact from "highcharts-react-official";
 
 //-= import map from "@highcharts/map-collection/custom/world.geo.json";
-import mapData from './maps/world';
+import mapData from "./maps/world";
+//-= import ReactTable from "react-table-6";
+//-= import "react-table-6/react-table.css";
+import ReactTable from "./components/ReactTable";
 
 HighchartsMap(Highcharts);
 
@@ -19,11 +23,14 @@ class MapaPrefixos extends React.Component {
 		this.chartRef = React.createRef();
 		this.state = {
 			prefix: '',
+			collector: '',
+			timestamp: '',
 			displayLabels: false,
 		}
 		
 		this.handleChange = this.handleChange.bind(this);
 		this.search = this.search.bind(this);
+		this.onFilteredChangeCustom = this.onFilteredChangeCustom.bind(this);
 		
 		this.options = {
 			chart: {
@@ -87,7 +94,7 @@ class MapaPrefixos extends React.Component {
 						enabled: this.state.displayLabels,
 					},
 				}, {
-					name: 'Visível com Prepend',
+					name: 'Visível com Origin Prepend',
 					type: 'mappoint',
 					marker: {
 						fillColor: 'BLUE',
@@ -103,10 +110,32 @@ class MapaPrefixos extends React.Component {
 	}
 	
 	setData(invisibles, visibles, prependeds) {
-		this.chartRef.current.chart.series[1].setData(invisibles, true);
-		this.chartRef.current.chart.series[2].setData(visibles, true);
-		this.chartRef.current.chart.series[3].setData(prependeds, true);
-		//-= this.chartRef.current.chart.update(this.options);
+		this.chartRef.current.chart.series[1].setData(visibles, true);
+		this.chartRef.current.chart.series[2].setData(prependeds, true);
+		this.chartRef.current.chart.series[3].setData(invisibles, true);
+		this.setState({invisibles: invisibles, visibles: visibles, prependeds: prependeds});
+	}
+	
+	onFilteredChangeCustom(value, accessor) {
+		let filtered = this.state.filtered ?? [];
+		let insertNewFilter = 1;
+
+		if (filtered.length) {
+			filtered.forEach((filter, i) => {
+				if (filter["id"] === accessor) {
+					if (value === "" || !value.length) filtered.splice(i, 1);
+					else filter["value"] = value;
+
+					insertNewFilter = 0;
+				}
+			});
+		}
+
+		if (insertNewFilter) {
+			filtered.push({ id: accessor, value: value });
+		}
+
+		this.setState({ filtered: filtered });
 	}
 	
 	handleChange(event) {
@@ -127,39 +156,219 @@ class MapaPrefixos extends React.Component {
 		fetch(`${this.urlApi}/collectors`).then(res => res.json()).then(collectors => {
 			if (!collectors?.length) return;
 			
-			fetch(`${this.urlApi}/resources?resources=${this.state.prefix}`).then(res => res.json()).then(data => {
-				let visibles = [], invisibles = [], prependeds = [];
-				collectors.forEach(collector => {
-					((!data.routes.some(r => r.collector === collector.id)) ? invisibles : 
-					(!data.prepends.some(r => r.collector === collector.id)) ? visibles : prependeds).push(
-						{
-							id: collector.id,
-							name: collector.name,
-							geographical: collector.location.geographical,
-							topological: collector.location.topological,
-							countryCode: collector.location.countryCode,
-							lat: collector.location.latitude,
-							lon: collector.location.longitude,
-						}
-					);
-				});
-				this.setData(invisibles, visibles, prependeds);
+			let collectorFilter = (this.state.collector) ? `&collectors=${this.state.collector}` : '';
+			let timestampFilter = (this.state.timestamp) ? `&timestamp=${this.state.timestamp}` : '';
+			
+			fetch(`${this.urlApi}/resources?resources=${this.state.prefix}${collectorFilter}${timestampFilter}`).then(res => res.json()).then(data => {
+				this.process(collectors, data);
 			});
 		});
 	}
 	
+	process(collectors, resources) {
+		let visibles = [], invisibles = [], prependeds = [], routes = resources.routes;
+		routes.forEach(r => {
+			r.prepends = [];
+			r.originPrepends = [];
+			r.poisonPrepends = [];
+			let origin = r.path[r.path.length - 1];
+			for (let i = r.path.length - 1; i >= 0; i--) {
+				let current = r.path[i];
+				for (let j = i - 1; j >= 0; j--) {
+					if (r.path[j] == current) {
+						if (current == origin) r.originPrepends.push(current);
+						if (j < i - 1) r.poisonPrepends.push(current);
+						r.prepends.push(current);
+						break;
+					}
+				}
+			}
+		});
+		
+		collectors.forEach(collector => {
+			if (this.state.collector && !this.state.collector.split(',').includes(`${collector.id}`)) return;
+			let v = routes.filter(r => r.collector === collector.id);
+			(!v.length ? invisibles : v.some(r => !r.originPrepends.length) ? visibles : prependeds).push(
+				{
+					id: collector.id,
+					name: collector.name,
+					geographical: collector.location.geographical,
+					topological: collector.location.topological,
+					countryCode: collector.location.countryCode,
+					lat: collector.location.latitude,
+					lon: collector.location.longitude,
+				}
+			);
+		});
+		this.setData(invisibles, visibles, prependeds);
+		this.setState({routes: routes});
+	}
+	
 	render() {
+		
+		this.options = {
+			chart: {
+				height: 700
+			},
+		
+			title: {
+				text: "Mapa Mundi"
+			},
+		
+			subtitle: {
+				text: "Visibilidade do prefixo"
+			},
+		
+			legend: {
+				align: 'left',
+				layout: 'vertical',
+				floating: true,
+			},
+		
+			mapNavigation: {
+				enabled: true,
+			},
+		
+			tooltip: {
+				animation: false,
+				formatter: function () {
+					return `${this.point.name} - ${this.point.geographical} (${this.point.countryCode})` + (
+						this.point.lat ? '<br>Lat: ' + this.point.lat + ' Lon: ' + this.point.lon : '');
+				}
+			},
+		
+			series: [
+				{
+					name: 'World',
+					mapData: mapData,
+					borderColor: '#707070',
+					nullColor: 'rgba(200, 200, 200, 0.3)',
+					showInLegend: false,
+				}, {
+					name: `Visível (${this.state.visibles?.length ?? 0})`,
+					type: 'mappoint',
+					marker: {
+						fillColor: 'GREEN',
+						symbol: 'circle',
+					},
+					dataLabels: {
+						format: '{point.id}',
+						enabled: this.state.displayLabels,
+					},
+					data: this.state.visibles,
+				}, {
+					name: `Visível com Prepend (${this.state.prependeds?.length ?? 0})`,
+					type: 'mappoint',
+					marker: {
+						fillColor: 'BLUE',
+						symbol: 'circle',
+					},
+					dataLabels: {
+						format: '{point.id}',
+						enabled: this.state.displayLabels,
+					},
+					data: this.state.prependeds,
+				}, {
+					name: `Não Visível (${this.state.invisibles?.length ?? 0})`,
+					type: 'mappoint',
+					marker: {
+						fillColor: 'RED',
+						symbol: 'circle',
+					},
+					dataLabels: {
+						format: '{point.id}',
+						enabled: this.state.displayLabels,
+					},
+					data: this.state.invisibles,
+				}
+			]
+		};
+		
+		const Genres = ({ values }) => {
+			return (
+				<>
+					{values.map((genre, idx) => {
+						return (
+							<span key={idx} className="badge">
+								{genre}
+							</span>
+						);
+					})}
+				</>
+			);
+		};
+		
+		const columns = [
+			{
+				Header: "Coletor",
+				accessor: "collector",
+			},
+			{
+				Header: "Caminho",
+				accessor: "path",
+				Cell: ({ cell: { value } }) => value,
+			},
+			{
+				Header: "Comunidades",
+				accessor: "community",
+				Cell: ({ cell: { value } }) => <Genres values={value} />,
+			},
+			{
+				Header: "Origin Prepends",
+				accessor: "originPrepends.length",
+			},
+			{
+				Header: "Poison Prepends",
+				accessor: "poisonPrepends.length",
+			},
+			{
+				Header: "Total Prepends",
+				accessor: "prepends.length",
+			}
+		];
+		
+		const data = this.state.routes ?? [];
+		const filtered = this.state.routesFiltereds ?? [];
+		
 		return (
 			<div>
 				<form onSubmit={this.search} style={{padding: '20px', textAlign: 'center'}} >
-					<label>Prefixo</label>
-					<input name="prefix" value={this.state.prefix} required onChange={this.handleChange} style={{margin: '5px'}} />
+					<label htmlFor="prefix">Prefixo</label>
+					<input id="prefix" name="prefix" value={this.state.prefix} required onChange={this.handleChange} style={{margin: '5px'}} />
+					<label htmlFor="collector">Coletor</label>
+					<input id="collector" name="collector" value={this.state.collector} onChange={this.handleChange} style={{margin: '5px'}} />
+					<label htmlFor="timestamp">Timestamp</label>
+					<input id="timestamp" name="timestamp" value={this.state.timestamp} onChange={this.handleChange} style={{margin: '5px'}} />
 					<button type="submit">Buscar</button>
-					<input name="displayLabels" type="checkbox" checked={this.state.displayLabels} onChange={this.handleChange} />
+					<input id="displayLabels" name="displayLabels" type="checkbox" checked={this.state.displayLabels} onChange={this.handleChange} style={{margin: '0px 5px 0px 10px'}} />
+					<label htmlFor="displayLabels">Exibir rótulos</label>
 				</form>
 				<HighchartsReact highcharts={Highcharts} options={this.options} constructorType={"mapChart"} ref={this.chartRef} />
+				<ReactTable
+					data={data}
+					columns={columns}
+					// filterable
+					// filtered={filtered}
+					// onFilteredChange={(filtered, column, value) => {
+					// 	this.onFilteredChangeCustom(value, column.id || column.accessor);
+					// }}
+					// defaultFilterMethod={(filter, row, column) => {
+					// 	const id = filter.pivotId || filter.id;
+					// 	if (typeof filter.value === "object") {
+					// 		return row[id] !== undefined
+					// 			? filter.value.indexOf(row[id]) > -1
+					// 			: true;
+					// 	} else {
+					// 		return row[id] !== undefined
+					// 			? String(row[id]).indexOf(filter.value) > -1
+					// 			: true;
+					// 	}
+					// }}
+					// className="-striped -highlight"
+				/>
 			</div>
 		);
 	}
 }
+
 render(<MapaPrefixos />, document.getElementById("root"));
